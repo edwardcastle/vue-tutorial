85defdc59671699e3a4662f1164dd212cc810f37
edwardcastle <sir.edwardcastle@gmail.com>
2022-07-25 23:11:55 -0400

    just adding some code

---
+++ src/components/Index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <p>{{'@' + fullName }}</p>
+      <p>followers: {{followers}}</p>
+      <button></button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: 'edwardcastle',
+        firstName: 'Edward',
+        lastName: 'Castle',
+        email: 'edwardcastle@gmail.com',
+        isAdmin: true
+      }
+    }
+  },
+  computed: {
+    fullName () {
+      return `${this.user.firstName} ${this.user.lastName}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div><p>sdfj</p></div>
+</template>
+
+<script>
+export default {
+  name: 'Test'
+}
+</script>
+
+<style scoped>
+
+</style>
